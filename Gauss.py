c426412759d6736df96db4bc613b18d820fadbd9
Oscar <odpobladorp@correo.udistrital.edu.co>
2022-11-12 12:00:42 -0500

    multiplicar

--- Gauss.py
+++ Gauss.py
@@ -7,13 +7,32 @@ class matriz:
     def __repr__(self):
         return str(self.listas)
     def multiplicar(self,aux,num_fila):
+        '''
+        Retorna una nueva fila en la que se multiplicó el número de fila indicado por el valor de aux ingresado
+        >>> m_1=matriz([1,2,3],[4,5,6],[7,8,9])
+        >>> m_1
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        >>> r_1=m_1.multiplicar(5,1)
+        >>> r_1
+        [[1, 2, 3], [20, 25, 30], [7, 8, 9]]
+        >>> r_1=m_1.multiplicar(8,2)
+        >>> r_1
+        [[1, 2, 3], [4, 5, 6], [56, 64, 72]
+        >>> r_1=m_1.multiplicar(0.5,1)
+        >>> r_1
+        [[0.5, 1.0, 1.5], [4, 5, 6], [7, 8, 9]]
+        '''
         renglon_multiplicar=[]
         for i in range(0,self.filas):
-            renglon_multiplicar[i]=self.listas[num_fila]*aux
-        return renglon_multiplicar
-        '''
-        >>> m_1=matriz([1,2,3],[4,5,6],[7,8,9])
-        '''
-m_1=matriz([[1,2,3],[4,5,6],[7,8,9]])
+            renglon_multiplicar.append(self.listas[num_fila][i]*aux)
+        matriz_multiplicar=self.listas
+        matriz_multiplicar[num_fila]=renglon_multiplicar
+        return matriz_multiplicar
+        
+m_1=matriz([[1,2,3]
+            ,[4,5,6]
+            ,[7,8,9]])
 print(m_1)
-#r_1=m_1.multiplicar(2,0)
+r_1=m_1.multiplicar(5,0)
+print(r_1)
+print(m_1)
