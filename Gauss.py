1d24e236548f08c5b3908ee107923845557f267d
JuanDavidR1 <judbellor@correo.udistrital.edu.co>
2022-11-12 11:05:44 -0500

    archivo

--- Gauss.py
+++ Gauss.py
@@ -1,19 +1,18 @@
 class matriz:
-    
     def __init__(self,matriz_A):
         self.listas=matriz_A
         self.filas=len(matriz_A)
         self.columnas=len(matriz_A[0])
-    def __repr__(self):
-        return str(self.listas)
-    def multiplicar(self,aux,num_fila):
-        renglon_multiplicar=[]
-        for i in range(0,self.filas):
-            renglon_multiplicar[i]=self.listas[num_fila]*aux
-        return renglon_multiplicar
-        '''
-        >>> m_1=matriz([1,2,3],[4,5,6],[7,8,9])
-        '''
-m_1=matriz([[1,2,3],[4,5,6],[7,8,9]])
-print(m_1)
-#r_1=m_1.multiplicar(2,0)
+        
+    def reduccion(self, i, j): #i es el indice del renglon que se le va a multiplicar, j es el indice del renglon a cambiar
+        matriz_principal=self.listas
+        rengloni=matriz_principal[i]
+        renglon=k*rengloni
+        renglon_cambiarj=matriz_principal[j]
+        renglon_cambiar=renglon_cambiar+renglon
+        matriz_borrar=matriz_principal.remove(renglon_cambiarj)
+        matriz_reduccion=matriz_principal.insert(j, renglon_cambiar)
+        print(matriz_reduccion)
+        return matriz_reduccion
+        #matriz_reduccion
+
