f972708b42e3c937ef56bf72590c0c5dbb6b332b
WillianChaparro <114821097+WillianChaparro@users.noreply.github.com>
2022-11-12 12:10:33 -0500

    Update Gauss.py

--- Gauss.py
+++ Gauss.py
@@ -14,12 +14,33 @@ class matriz:
         '''
         >>> m_1=matriz([1,2,3],[4,5,6],[7,8,9])
         '''
-     def cambiafila(self,inicio,llegada):
+    def cambiafila(self,inicio,llegada):
+        '''
+        Cambiafila cambio de posicion dos filas inicio a llegada y viceversa
+        ----------------
+        >>> m1=matriz([[3,1,2],[4,5,6],[3,7,9]])
+        
+        >>> m1
+        [[3, 1, 2], [4, 5, 6], [3, 7, 9]]
+        >>> m2=m1.cambiafila(0,1)
+        
+        >>> m2
+        [[4, 5, 6], [3, 1, 2], [3, 7, 9]]
+        >>> m3=m1.cambiafila(1,2)
+        
+        >>> m3
+        [[4, 5, 6], [3, 7, 9], [3, 1, 2]]
+        >>> m4=m1.cambiafila(0,2)
+        
+        >>> m4
+        [[3, 1, 2], [3, 7, 9], [4, 5, 6]]
+        '''
         filaInic=self.listas[inicio]
         filaLleg=self.listas[llegada]
         matriz_B=self.listas[llegada]=filaInic
         matriz_B=self.listas[inicio]=filaLleg
         return self.listas
+
 m_1=matriz([[1,2,3],[4,5,6],[7,8,9]])
 print(m_1)
 #r_1=m_1.multiplicar(2,0)
